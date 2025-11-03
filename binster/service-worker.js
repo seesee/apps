65e74a931b71a6c168e57e4102309424338e2686
Chris Carline <chris@carline.org>
2025-11-03 14:26:12 +0000

    add binster v1.0

---
+++ binster/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("bin-generator-v1").then((cache) => {
+      return cache.addAll(["./", "index.html", "manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
